a9b3826be717ee8b23ba69b40ef95405a9139ae4
echobanda <echobanda@gmail.com>
2017-08-27 18:37:29 +0200

    change exp section

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 require('../main.scss')
 
-var button = $('.burger').find(".button");
-console.log(button);
+// ===== Scroll to Top ====
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('.return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('.return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('.return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
 
-button.on('click',function(){
-  $('.sovdev').slideToggle(function () {
-    $('.sovdev').css("visibility", "visible");
-  });
+
 
 });
